bdaa60bd5d7de4b1d31e1d580a727f67323a3b31
BookTS <sachtran1811@gmail.com>
2023-04-09 23:45:42 +0700

    Update 11:45 | 09/04/2023

---
+++ B3/src/yeu_cau_1.cpp
@@ -0,0 +1,62 @@
+// #include <Arduino.h>
+
+// #define K0 42
+// #define K1 47
+// #define K2 46
+// #define K3 43
+// #define COM 33
+
+// char PinOut[] = {22, 23, 24, 25, 26, 27, 28, 29};
+
+// void setPinOut(char temp);
+
+// void setup() {
+//   Serial.begin(9600);
+
+//   for (int i = 0; i < 8; i++)
+//     pinMode(PinOut[i], OUTPUT);
+
+//   pinMode(COM, OUTPUT);
+//   digitalWrite(COM, LOW);
+
+//   pinMode(K0, INPUT_PULLUP);
+//   pinMode(K1, INPUT_PULLUP);
+//   pinMode(K2, INPUT_PULLUP);
+//   pinMode(K3, INPUT_PULLUP);
+// }
+
+// void loop() {
+//   if (Serial.available()) {
+//     char ch = Serial.read();
+//     setPinOut(ch);
+//   }
+
+//   if (digitalRead(K0) == 0) {
+//     Serial.println('0');
+//     while(!digitalRead(K0));
+//   }
+//   if (digitalRead(K1) == 0) {
+//     Serial.println('1');
+//     while(!digitalRead(K1));
+//   }
+//   if (digitalRead(K2) == 0) {
+//     Serial.println('2');
+//     while(!digitalRead(K2));
+//   }
+//   if (digitalRead(K3) == 0) {
+//     Serial.println('3');
+//     while(!digitalRead(K3));
+//   }
+//   delay(5);
+// }
+
+// // Set chan theo hex
+// void setPinOut(char temp) {
+//   for (int i = 0; i < 8; i++) {
+//     if (temp & 0x01)
+//       digitalWrite(PinOut[i], HIGH);
+//     else
+//       digitalWrite(PinOut[i], LOW);
+//     temp = temp >> 1;
+//   }
+// }
---
+++ B3/src/yeu_cau_3.cpp
@@ -0,0 +1,74 @@
+// #include <Arduino.h>
+// #include <LiquidCrystal.h>
+
+// LiquidCrystal lcd(40, 41, 37, 36, 35, 34);
+
+// #define K1 47
+// #define K2 46
+// #define K3 43
+// #define K4 47
+// #define LR  13
+// #define LG  11
+// #define LB  12
+
+// #define LM35 A0
+// #define PHOTO A7
+
+// void setup()
+// {
+//     pinMode(K1, INPUT_PULLUP);
+//     pinMode(K2, INPUT_PULLUP);
+//     pinMode(K3, INPUT_PULLUP);
+//     pinMode(K4, INPUT_PULLUP);
+//     pinMode(LR, OUTPUT);
+//     pinMode(LG, OUTPUT);
+//     pinMode(LB, OUTPUT);
+
+//     lcd.begin(16, 2);
+//     lcd.setCursor(0, 0);
+//     lcd.print("Hello world ! ");
+//     lcd.setCursor(0, 1);
+//     lcd.print("DLDK INSEE ");
+//     delay(500);
+//     lcd.clear();
+
+//     Serial.begin(9600);
+//     delay(200);
+//     Serial.println("Hello world !");
+// }
+
+// void loop()
+// {
+//     int tmp = map(analogRead(LM35), 0, 1023, 0, 5000) / 10;
+//     int lux = analogRead(PHOTO);
+//     lcd.setCursor(0, 1);
+//     lcd.print("T: ");
+//     lcd.setCursor(10, 1);
+//     lcd.print("P: ");
+//     lcd.setCursor(0, 1);
+//     lcd.print(tmp);
+//     lcd.setCursor(10, 1);
+//     lcd.print(lux);
+
+//     Serial.println("T" + String(tmp) + "P" + String(lux));
+
+//     if (Serial.available() > 0)
+//     {
+//         String str = Serial.readString();
+//         lcd.setCursor(0, 1);
+//         lcd.print(" ");
+//         lcd.setCursor(0, 1);
+//         lcd.print(str);
+
+//         String cmd = str.substring(0, 1);
+//         String val = str.substring(1, str.length() - 1);
+
+//         if (cmd == "R")
+//             analogWrite(LR, val.toInt());
+//         if (cmd == "G")
+//             analogWrite(LG, val.toInt());
+//         if (cmd == "B")
+//             analogWrite(LB, val.toInt());
+//     }
+//     delay(100);
+// }
